e51f43cbcd6d23c374f7df07f7f11a6de052f5ab
Shreyash Kumar Singh <141475077+shing1Sks@users.noreply.github.com>
2024-06-13 20:19:55 +0530

    fix

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import HomeSec from "@/components/HomeSec";
 
 export const metadata = {
-  title: "Know my Roads !",
+  title: "Know Your Roads !",
   description: "Know Your Roads (KYR) is an innovative solution designed to bring you the real story about the roads you travel every day. We believe that informed drivers are safe drivers, and our platform empowers you to make better decisions by giving you access to the latest information on road conditions.",
 };
 export default function Home() {
   return (
     <div>
+      <link rel="shortcut icon" href="./kyr.png" type="png" />
       <HomeSec />
     </div>
   );
