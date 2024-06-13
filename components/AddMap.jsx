8fb333e89a5ad175ffa746c4d4a46f478b65eca8
Shreyash Kumar Singh <141475077+shing1Sks@users.noreply.github.com>
2024-06-14 00:24:11 +0530

    feature added get current location

--- components/AddMap.jsx
+++ components/AddMap.jsx
@@ -4,9 +4,9 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 const AddMap = forwardRef(function AddMap(props, ref) {
   const markerRef = ref;
+  let map;
   useEffect(() => {
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
-    let map;
     const initializeMap = (lng, lat) => {
       map = new mapboxgl.Map({
         container: "addmap",
@@ -55,6 +55,34 @@ const AddMap = forwardRef(function AddMap(props, ref) {
       }
     };
   }, []);
+  const handleGetLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lng = position.coords.longitude;
+          const lat = position.coords.latitude;
+          if (map) {
+            map.flyTo({
+              center: [lng, lat],
+              essential: true,
+            });
+            if (markerRef.current) {
+              markerRef.current.remove();
+            }
+            const newMarker = new mapboxgl.Marker({
+              color: "#0c1f3d",
+            })
+              .setLngLat([lng, lat])
+              .addTo(map);
+            markerRef.current = newMarker;
+          }
+        },
+        () => {
+          alert("Unable to retrieve your location");
+        }
+      );
+    }
+  };
   return (
     <div>
       <div className="relative w-[95vw] sm:w-[50vw] mt-4 h-[200px] sm:h-[300px] rounded-md mx-auto bg-slate-200 dark:bg-blue-800">
@@ -62,6 +90,12 @@ const AddMap = forwardRef(function AddMap(props, ref) {
           id="addmap"
           className="absolute border-2 border-blue-800 rounded-md dark:border-gray-600 top-0 left-0 w-full h-full"
         ></div>
+        <button
+          onClick={handleGetLocation}
+          className="bg-slate-600 rounded-full absolute left-2 top-2 z-10"
+        >
+          📍
+        </button>
       </div>
     </div>
   );
