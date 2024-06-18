2ac610bb85556d702066cbb4cfdfc8d151de7fbf
Shreyash Kumar Singh <141475077+shing1Sks@users.noreply.github.com>
2024-06-18 19:53:42 +0530

    clg removed

--- tools/addRoad.js
+++ tools/addRoad.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function addRoad(data) {
     try {
-        console.log(data);
+        // console.log(data);
         const res = await axios.post(`${process.env.NEXT_PUBLIC_SERVER}/api/roads/upload-road`,data,{
             headers: {
                 "Content-Type": "multipart/form-data",
